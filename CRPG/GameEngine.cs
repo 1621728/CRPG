a362ce498a9db6c36f135893ad0f23f9792a8cc2
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-05-23 13:13:49 -0700

    Finished version 4.

--- CRPG/GameEngine.cs
+++ CRPG/GameEngine.cs
@@ -6,7 +6,9 @@ namespace CRPG
 {
     public static class GameEngine
     {
-        public static string Version = "0.0.3";
+        public static string Version = "0.0.4";
+        public static Monster _currentMonster;
+
 
         public static void Initialize()
         {
@@ -14,6 +16,15 @@ namespace CRPG
             Console.WriteLine("\n\nWelcome to the World of {0}", World.WorldName);
             Console.WriteLine();
             World.ListLocations();
+            if(_currentMonster != null)
+            {
+                Console.WriteLine("Current Monster:{0}", _currentMonster);
+            }
+            else
+            {
+                Console.WriteLine("No Current Monster");
+            }
+            
 
         }
 
@@ -26,30 +37,106 @@ namespace CRPG
 
         }
 
-        public static void QuestProcessor(Player player, Location location)
+        public static void QuestProcessor(Player _player, Location newLocation)
         {
-            if(location.QuestAvailableHere != null)
+            string questMessage;
+            // Does the location have a quest?
+            if (newLocation.QuestAvailableHere != null)
             {
-                bool playerAlreadyHasQuest = false;
-                bool playerAlreadyCompletedQuest = false;
-                Console.WriteLine("Debug: Q is here!::::::");
-                Console.WriteLine("\t{0}", location.QuestAvailableHere.Description);
+                // See if the player already has the quest, and if they've completed it
+                bool playerAlreadyHasQuest = _player.HasThisQuest(newLocation.QuestAvailableHere);
+                bool playerAlreadyCompletedQuest = _player.CompletedThisQuest(newLocation.QuestAvailableHere);
 
-                foreach ( PlayerQuest playerQuest in player.Quests)
+                // See if the player already has the quest
+                if (playerAlreadyHasQuest)
                 {
-                    if(playerQuest.Details.ID == location.QuestAvailableHere.ID)
+                    // If the player has not completed the quest yet
+                    if (!playerAlreadyCompletedQuest)
                     {
-                        playerAlreadyHasQuest = true;
+                        // See if the player has all the items needed to complete the quest
+                        bool playerHasAllItemsToCompleteQuest = _player.HasAllQuestCompletionItems(newLocation.QuestAvailableHere);
 
-                        if (playerQuest.IsCompleted)
+                        // The player has all items required to complete the quest
+                        if (playerHasAllItemsToCompleteQuest)
                         {
-                            playerAlreadyCompletedQuest = true;
+                            // Display message
+                            questMessage = Environment.NewLine;
+                            questMessage += "You complete the '" + newLocation.QuestAvailableHere.Name + "' quest." + Environment.NewLine;
+
+                            // Remove quest items from inventory
+                            _player.RemoveQuestCompletionItems(newLocation.QuestAvailableHere);
+
+                            // Give quest rewards
+                            questMessage += "You receive: " + Environment.NewLine;
+                            questMessage += newLocation.QuestAvailableHere.RewardExperiencePoints.ToString() + " experience points" + Environment.NewLine;
+                            questMessage += newLocation.QuestAvailableHere.RewardGold.ToString() + " gold" + Environment.NewLine;
+                            questMessage += newLocation.QuestAvailableHere.RewardItem.Name + Environment.NewLine;
+                            questMessage += Environment.NewLine;
+                            Console.WriteLine(questMessage);
+
+                            _player.ExperiencePoints += newLocation.QuestAvailableHere.RewardExperiencePoints;
+                            _player.Gold += newLocation.QuestAvailableHere.RewardGold;
+
+                            // Add the reward item to the player's inventory
+                            _player.AddItemToInventory(newLocation.QuestAvailableHere.RewardItem);
+
+                            // Mark the quest as completed
+                            _player.MarkQuestCompleted(newLocation.QuestAvailableHere);
                         }
                     }
-                }//Foreach
+                }
+                else
+                {
+                    // The player does not already have the quest
+
+                    // Display the messages
+                    questMessage = "You receive the " + newLocation.QuestAvailableHere.Name + " quest." + Environment.NewLine;
+                    questMessage += newLocation.QuestAvailableHere.Description + Environment.NewLine;
+                    questMessage += "To complete it, return with:" + Environment.NewLine;
+                    foreach (QuestCompletionItem qci in newLocation.QuestAvailableHere.QuestCompletionItems)
+                    {
+                        if (qci.Quantity == 1)
+                        {
+                            questMessage += qci.Quantity.ToString() + " " + qci.Details.Name + Environment.NewLine;
+                        }
+                        else
+                        {
+                            questMessage += qci.Quantity.ToString() + " " + qci.Details.NamePlural + Environment.NewLine;
+                        }
+                    }
+                    questMessage += Environment.NewLine;
+                    Console.WriteLine(questMessage);
+
+                    // Add the quest to the player's quest list
+                    _player.Quests.Add(new PlayerQuest(newLocation.QuestAvailableHere, false));
+                }
+            }
+
+        } //QuestProcessor
 
 
-            }//location.QuestAvailable here.
-        }//QuestProcessor
+        public static void MonsterProcessor(Player _player, Location newLocation)
+        {
+            string monsterMessage = "";
+            //Does this location have a monster?
+            if(newLocation.MonsterLivingHere != null)
+            {
+                monsterMessage += "You see a " + newLocation.MonsterLivingHere.Name + "\n";
+                Console.WriteLine(monsterMessage);
+                //Make a new Monster
+                Monster standardMonster = World.MonsterByID(newLocation.MonsterLivingHere.ID);
+                _currentMonster = new Monster(standardMonster.ID, standardMonster.Name, standardMonster.MaximumDamage, standardMonster.RewardExperiencePoints,
+                    standardMonster.RewardGold, standardMonster.CurrentHitPoints, standardMonster.MaximumHitPoints);
+
+                foreach(LootItem lootItem in standardMonster.LootTable)
+                {
+                    _currentMonster.LootTable.Add(lootItem);
+                }
+            }
+            else
+            {
+                _currentMonster = null;
+            }
+        }
     }
 }
